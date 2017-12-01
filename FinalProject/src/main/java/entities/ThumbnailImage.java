a43acc310daf308d60848c3c56fa49d60407112c
Nikita Guchenko <guchenko.n@gmail.com>
2017-12-01 16:15:23 +0200

    Added thumbnails

---
+++ FinalProject/src/main/java/entities/ThumbnailImage.java
@@ -0,0 +1,10 @@
+package entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ThumbnailImage {
+    public String url;
+    public int width;
+    public int height;
+}
